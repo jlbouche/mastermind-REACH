3c5a71d6948573eae4005e07c449abb2999f1d8a
jlbouche <jennalbouche@gmail.com>
2022-04-07 14:39:47 -0700

    fix axios call and return of data for integers as individual array items

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //define constants
 const maxGuesses = 10;
-const apiURL = "https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new";
 
 //define variable elements
+let randomNumbers = [];
 let guessesCount = 0;
 let currentGuess = [];
 let prevousGuesses = [];
@@ -11,18 +11,22 @@ let scoreCount = {
     losses: 0,
 }
 
-async function getRandomNumbers(url){
+async function getRandomNumbers(){
     axios.get("https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new")
-    .then((response) => {
-        console.log(response.data)
-    })
+        .then((response) => {
+            let data = response.data.split("");
+            randomNumbers = data.filter(el => el !== '\n')
+            console.log(randomNumbers)
+        })
 }
+
+getRandomNumbers();
 
 
 function winLoss(){
-    if (guessesCount === maxGuesses){
-        //lost game modal
-    } else if (currentGuess === correctAnswer){
+    if (currentGuess === correctAnswer){
         //won game modal
+    } else if (guessesCount === maxGuesses){
+        //lost game modal
     }
 }
