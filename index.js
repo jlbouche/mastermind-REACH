fbd7e38de7b6e388e048eb3238161b9518261b8a
jlbouche <jennalbouche@gmail.com>
2022-04-08 12:13:46 -0700

    fix error in parseint of api call, fix conditional in win condition to compare array values

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //define constants
 const startNewGame = document.getElementById('start-game');
 const maxGuesses = 10;
-const numbers = document.querySelectorAll("#numbers-grid")
+const numbers = document.querySelectorAll(".number")
 
 //define variable elements
 let randomNumbers = [];
@@ -16,7 +16,7 @@ let scoreCount = {
 //event listeners
 startNewGame.addEventListener('click', startGame);
 for (let number of numbers){
-    let guess = number
+    let guess = number.innerHTML
     number.addEventListener('click', () => guessAnswer(guess));
 }
 
@@ -24,19 +24,22 @@ async function getRandomNumbers(){
     axios.get("https://www.random.org/integers/?num=4&min=0&max=7&col=1&base=10&format=plain&rnd=new")
         .then((response) => {
             let data = response.data.split("");
-            randomNumbers = data.filter(el => el !== '\n')
+            let stringNumbers = data.filter(el => el !== '\n')
+            randomNumbers = stringNumbers.map(function (x){
+                return parseInt(x);
+            })
             console.log(randomNumbers)
         })
 }
 
 function guessAnswer(guess){
-    currentGuess.push(guess)
+    currentGuess.push(parseInt(guess))
+    console.log(currentGuess)
     console.log(`hitting guessAnswer function pre 4 guesses, currentGuess updated to ${currentGuess}`)
     if (currentGuess.length === 4){
         guessesCount++;
         console.log(`hitting guessAnswer conditional of 4 numbers in guess, guessesCount now ${guessesCount}`)
         winLoss();
-        currentGuess = [];
     }
 }
 
@@ -45,7 +48,8 @@ function startGame(){
 }
 
 function winLoss(){
-    if (currentGuess === randomNumbers){
+    console.log(currentGuess, randomNumbers)
+    if (currentGuess.every((val, idx) => val === randomNumbers[idx])){
         //won game modal
         console.log('hitting winLoss won game condition')
     } else if (guessesCount === maxGuesses){
@@ -54,5 +58,6 @@ function winLoss(){
     } else {
         previousGuesses.push(currentGuess);
         console.log(`hitting winLoss add history of guesses, previousGuesses updated to: ${previousGuesses} and guessesCount increased to ${guessesCount}`)
+        currentGuess = [];
     }
 }
